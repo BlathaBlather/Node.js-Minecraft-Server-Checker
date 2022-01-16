f09597b99e9968c993b469e30814e36cd4af1205
BlathaBlather <32522738+BlathaBlather@users.noreply.github.com>
2022-01-16 18:13:29 +0000

    made a minecraft server checker

---
+++ main.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const mcping = require('mcping-js')
+
+// 25565 is the default Minecraft Java Edition multiplayer server port
+// The port may be omitted and will default to 25565
+
+const timeout = 5000;
+const protocolVersion = 757;
+
+function checkOnlineAndSend(io, socket_id, server_name){
+    var server = new mcping.MinecraftServer(server_name, 25565)
+    server.ping(timeout, protocolVersion, (err, res) => {
+        if (res) {
+            io.to(socket_id).emit("update", true);
+        } else {
+            io.to(socket_id).emit("update", false);
+        }
+    })
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('update', (server_name) => {
+        console.log('message: ' + server_name + " from " + socket.id);
+        checkOnlineAndSend(io, socket.id, server_name)
+    });
+});
+
+
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
